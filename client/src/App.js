801774fe65b6b3eeee310c226c34333dd2b75934
C.Dufficy <c.dufficy@gmail.com>
2023-06-13 10:52:51 +0100

    logo placeholder added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-// import logo from "src/weightlifting-logo.jpg";
+import logo from './weightlifting-logo.jpg';
 
 
 
 function App() {
     return (
         <div className="App">
-            <p>logo and nav bar to go here</p>
+            <img style={{ width: '100px', height: 'auto' }} src={logo} />
+            <p>nav bar to go here</p>
             <h1>Welcome to your Weight Mate Training App</h1>
         </div>
     );
