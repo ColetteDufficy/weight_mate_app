61852b8aeaa6271842e21ea955c78bea34a723b5
C.Dufficy <c.dufficy@gmail.com>
2023-07-17 11:53:38 +0100

    front end functioning. moving on to backend

--- client/src/services/ExerciseService.js
+++ client/src/services/ExerciseService.js
@@ -23,7 +23,7 @@ const ExerciseService = {
     },
 
 
-    // filtered fetch result that is based on the values submitted by the users choice in FilterExercise.js
+    // filtered fetch method that takes inthe values submitted by the users choice in FilterExercise.js
     filteredFetch(selectedType, selectedMuscle, selectedDifficulty) {
         return fetch(`${baseURL}?type=${selectedType}&muscle=${selectedMuscle}&difficulty=${selectedDifficulty}`, 
         { headers: { 'X-Api-Key': API_KEY }})
@@ -36,52 +36,3 @@ export default ExerciseService;
 
 
 
-
-
-
-// // ORIGINAL FETCH CODE
-// import { API_KEY } from '../env.js';
-// // console.log(API_KEY)
-
-// const baseURL = 'https://api.api-ninjas.com/v1/exercises';
-
-// const getAllExercises = () => {
-//     return fetch(baseURL, {
-//         headers: { 'X-Api-Key': API_KEY },
-//     })
-//         .then(res => res.json())
-// }
-
-// export default getAllExercises;
-
-
-
-// a function created to 'shuffle' the data thats retuerned from the fetch
-// const shuffleList = (list) => {
-//     for (let index = list.length - 1; index > 0; index--) {
-//         const randomIndexPosition = Math.floor(Math.random() * (index + 1));
-//         [list[index], list[randomIndexPosition]] = [list[randomIndexPosition], list[index]];
-//     }
-//     return list;
-// };
-
-
-
-//     // returns the intial 10 exercises from the API
-//     getAllExercises() {
-//         return fetch(baseURL, {
-//             headers: { 'X-Api-Key': API_KEY },
-//         })
-//             .then(res => res.json());
-//     },
-
-
-
-//     // return random list of exercises from the API
-//     randomListOfExercises() {
-//         return fetch(baseURL, {
-//             headers: {'X-Api-Key': API_KEY},
-//         })
-//         .then(res => res.json())
-//         .then((data) => shuffleList(data))
-//     },
